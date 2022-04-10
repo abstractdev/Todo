5401110701490ecae7fb4bdf8d4600b43227a823
abstractdev <fadcny@gmail.com>
2022-04-10 10:04:26 -0400

    Added renderTasks function to display

--- src/Display.js
+++ src/Display.js
@@ -96,6 +96,132 @@ function Display() {
       })();
     });
   }
+  function renderTasks(taskArray) {
+    while (mainContainer.firstChild) {
+      mainContainer.removeChild(mainContainer.firstChild);
+    }
+    taskArray.forEach((element) => {
+      const tasksDisplay = document.createElement("div");
+      tasksDisplay.classList.add("tasksDisplay");
+      tasksDisplay.setAttribute(
+        "data-id",
+        taskArray[taskArray.indexOf(element)].id.toString()
+      );
+      mainContainer.appendChild(tasksDisplay);
+      const tasksDisplayLeftContainer = document.createElement("div");
+      tasksDisplayLeftContainer.classList.add("tasksDisplayLeftContainer");
+      const tasksText = document.createElement("div");
+      tasksText.classList.add("tasksText");
+      tasksText.textContent = element.title;
+      const tasksCheckBox = document.createElement("div");
+      tasksCheckBox.classList.add("tasksCheckBox");
+      const tasksDisplayRightContainer = document.createElement("div");
+      tasksDisplayRightContainer.classList.add("tasksDisplayRightContainer");
+      const tasksEditIconContainer = document.createElement("div");
+      tasksEditIconContainer.classList.add("tasksEditIconContainer");
+      const tasksEditIcon = document.createElement("span");
+      tasksEditIcon.classList.add("fa-solid");
+      tasksEditIcon.classList.add("fa-pen");
+      tasksEditIcon.setAttribute("id", "tasksEditIcon");
+      const tasksDeleteButton = document.createElement("button");
+      tasksDeleteButton.classList.add("tasksDeleteButton");
+      tasksDeleteButton.textContent = "Del";
+      const dropdownContainer = document.createElement("div");
+      dropdownContainer.classList.add("dropdownContainer");
+      const dropdown = document.createElement("div");
+      dropdown.classList.add("dropdown");
+      const dropdownButton = document.createElement("button");
+      dropdownButton.classList.add("dropdownButton");
+      dropdownButton.textContent = "Priority";
+      const dropdownContent = document.createElement("div");
+      dropdownContent.classList.add("dropdownContent");
+      dropdownContent.setAttribute(
+        "data-dropdown",
+        taskArray[taskArray.indexOf(element)].id.toString()
+      );
+      const lowPriority = document.createElement("a");
+      lowPriority.textContent = "low";
+      lowPriority.href = "#";
+      lowPriority.classList.add("lowPriority");
+      const mediumPriority = document.createElement("a");
+      mediumPriority.textContent = "medium";
+      mediumPriority.href = "#";
+      mediumPriority.classList.add("mediumPriority");
+      const highPriority = document.createElement("a");
+      highPriority.textContent = "high";
+      highPriority.href = "#";
+      highPriority.classList.add("highPriority");
+      tasksDisplay.appendChild(tasksDisplayLeftContainer);
+      tasksDisplayLeftContainer.appendChild(tasksCheckBox);
+      tasksDisplayLeftContainer.appendChild(tasksText);
+      tasksDisplay.appendChild(tasksDisplayRightContainer);
+      tasksDisplayRightContainer.appendChild(tasksEditIconContainer);
+      tasksEditIconContainer.appendChild(tasksEditIcon);
+      tasksDisplayRightContainer.appendChild(dropdownContainer);
+      dropdownContainer.appendChild(dropdown);
+      dropdown.appendChild(dropdownButton);
+      dropdown.appendChild(dropdownContent);
+      dropdownContent.appendChild(lowPriority);
+      dropdownContent.appendChild(mediumPriority);
+      dropdownContent.appendChild(highPriority);
+      dropdownContainer.appendChild(dropdown);
+      tasksDisplayRightContainer.appendChild(tasksDeleteButton);
+
+      const addDropdownListeners = (() => {
+        dropdownButton.addEventListener("click", (e) => {
+          if (e.target.className === "dropdownButton") {
+            document
+              .querySelector(
+                `[data-dropdown="${taskArray[
+                  taskArray.indexOf(element)
+                ].id.toString()}"]`
+              )
+              .classList.toggle("showDropdown");
+          }
+        });
+      })();
+      const displayCompleteStatus = (() => {
+        if (taskArray[taskArray.indexOf(element)].complete === true) {
+          tasksText.style.textDecoration = "line-through";
+          tasksCheckBox.style.backgroundColor = "#32fbe2";
+        }
+      })();
+
+      const displayPriorityStatus = (() => {
+        switch (taskArray[taskArray.indexOf(element)].priority) {
+          case "low":
+            dropdownButton.style.backgroundColor = "green";
+            break;
+          case "medium":
+            dropdownButton.style.backgroundColor = "#ffc107";
+            break;
+          case "high":
+            dropdownButton.style.backgroundColor = "#e44c55";
+            break;
+        }
+      })();
+      const renderEditTaskModal = (() => {
+        tasksDisplay.addEventListener("click", (e) => {
+          if (
+            e.target.className === "tasksText" ||
+            e.target.id === "tasksEditIcon"
+          ) {
+            editTaskTitle.value = taskArray[taskArray.indexOf(element)].title;
+            editTaskDescription.value =
+              taskArray[taskArray.indexOf(element)].description;
+            editTaskDueDate.value =
+              taskArray[taskArray.indexOf(element)].dueDate;
+            editTaskNotes.value = taskArray[taskArray.indexOf(element)].notes;
+            editTaskModalContainer.classList.add("show");
+            editSubmitTask.setAttribute(
+              "data-id",
+              taskArray[taskArray.indexOf(element)].id.toString()
+            );
+          }
+        });
+      })();
+    });
+  }
 }
 
 export default Display;
