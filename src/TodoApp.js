243595383fca58b6e21254694e97a8b3df5ceec3
abstractdev <fadcny@gmail.com>
2022-04-10 20:13:11 -0400

    Can now add tasks and display them

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,6 +1,7 @@
 import {Display} from "./Display.js";
 import {Project} from "./Project.js"
 import {Task} from "./Task.js"
+import { createIdForArrayElements } from "./IdFunctions.js";
 
 export const TodoApp = () => {
   const projectsArray = [];
@@ -12,6 +13,17 @@ export const TodoApp = () => {
     ".project-modal-container"
   );
   const newTaskButton = document.querySelector(".new-task-button");
+  const submitTask = document.querySelector("#submit-task");
+  const taskTitle = document.querySelector("#title");
+  const editTaskTitle = document.querySelector("#edit-title");
+  const taskDescription = document.querySelector("#description");
+  const editTaskDescription = document.querySelector("#edit-description");
+  const taskDueDate = document.querySelector("#dueDate");
+  const editTaskDueDate = document.querySelector("#edit-dueDate");
+  const taskNotes = document.querySelector("#notes");
+  const editTaskNotes = document.querySelector("#edit-notes");
+  const taskForm = document.querySelector("#task-form");
+  const taskModalContainer = document.querySelector("#task-modal-container");
 
   const renderApp = () => {
     Display.renderTasks(tasksArray);
@@ -31,6 +43,7 @@ export const TodoApp = () => {
     function storeProjectDataInArray (title){
       const newProject = Project();
       projectsArray.push(newProject);
+      createIdForArrayElements(projectsArray);
       newProject.title = title;
       renderApp();
     }
@@ -44,5 +57,38 @@ export const TodoApp = () => {
       renderApp();
     })
   })()
-}
 
+  function storeTaskDataInArray(title, description, dueDate, notes) {
+      let newTask = Task();
+      newTask.title = title;
+      // newTask.projectId = this.model.currentProject.id;
+      newTask.description = description;
+      newTask.dueDate = dueDate;
+      newTask.notes = notes;
+      // model.currentProject.array.push(newTask);
+      tasksArray.push(newTask);
+      createIdForArrayElements(tasksArray);
+      renderApp();
+      // if (view.mainContainerTopText.textContent === "All Tasks") {
+      //   view.renderTasks(taskArray);
+      // } else if (view.mainContainerTopText.textContent === "Today") {
+      //   view.renderTasks(todayTaskArray);
+      // } else {
+      //   view.renderTasks(currentProject.array);
+      // }
+  }
+
+  const handleTaskEventListener = (() => {
+    submitTask.addEventListener("click", (e) => {
+      e.preventDefault();
+      storeTaskDataInArray(
+        taskTitle.value,
+        taskDescription.value,
+        taskDueDate.value,
+        taskNotes.value
+      );
+      taskForm.reset();
+      taskModalContainer.classList.remove("show");
+    });
+  })()
+}
