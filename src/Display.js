f8e2bb983085436f083c4eeb512eacb3e2c5ee14
abstractdev <fadcny@gmail.com>
2022-04-13 12:57:42 -0400

    merged event listeners with display

--- src/Display.js
+++ src/Display.js
@@ -28,48 +28,54 @@ export const Display = (() => {
   const editTaskDueDate = document.querySelector("#edit-dueDate");
   const taskNotes = document.querySelector("#notes");
   const editTaskNotes = document.querySelector("#edit-notes");
-  const mainContainer = document.querySelector(".main-container");
-  const mainContainerTopTextContainer = document.querySelector(
-    ".main-container-top-text-container"
-  );
-  const mainContainerTopText = document.querySelector(
-    ".main-container-top-text"
-  );
+  const sidebarContainer = document.querySelector(".sidebar-container")
   const projectsContainer = document.querySelector(".projects-container");
   const allProjects = document.querySelector("#all-projects");
   const allTasks = document.querySelector("#all-tasks");
   const container = document.querySelector(".container");
   const taskModal = document.querySelector(".task-modal");
   const todayButton = document.querySelector(".today-button");
-  const mainContentContainer = document.querySelector(".main-content-container")
+  const mainContainer = document.querySelector(".main-container");
+  const mainContentContainer = document.querySelector(".main-content-container");
+  const mainProjectsContainer = document.querySelector(".main-projects-container");
+  const mainTasksContainer = document.querySelector(".main-tasks-container");
+  const submitProject = document.querySelector("#submit-project");
+const projectForm = document.querySelector("#project-form");
+const projectTitle = document.querySelector("#project-title");
+const topText = document.querySelector(".top-text");
+let currentProjectId;
+
+function initCurrentProject(){
+  if (!currentProjectId) {
+    currentProjectId = 'Default'
+  }
+}
 
   return {
-    renderAllProjects: (projectsArray) => {
-      while (mainContainer.firstChild) {
-        mainContainer.removeChild(mainContainer.firstChild);
+    initCurrentProject,
+    renderAllProjects(projectsArray) {
+      while (mainContentContainer.firstChild) {
+        mainContentContainer.removeChild(mainContentContainer.firstChild);
       }
       projectsArray.forEach((element) => {
-        const projectsDisplay = document.createElement("div");
+       const projectsDisplay = document.createElement("div");
         projectsDisplay.classList.add("projectsDisplay");
         projectsDisplay.setAttribute(
           "data-id",
-          projectsArray[projectsArray.indexOf(element)].id.toString()
-        );
-        mainContainer.appendChild(projectsDisplay);
+          element.id.toString());
+        mainContentContainer.appendChild(projectsDisplay);
         const projectsText = document.createElement("div");
         projectsText.classList.add("projectsText");
         projectsText.textContent = element.title;
-        projectsDisplay.appendChild(projectsText);
-        const projectsDisplayRightContainer = document.createElement("div");
-        projectsDisplayRightContainer.classList.add(
-          "projectsDisplayRightContainer"
-        );
-        const projectsEditIconContainer = document.createElement("div");
-        projectsEditIconContainer.classList.add("projectsEditIconContainer");
-        const projectsEditIcon = document.createElement("i");
+        projectsDisplay.appendChild(projectsText)
+        const projectsDisplayRightContainer = document.createElement('div');
+        projectsDisplayRightContainer.classList.add('projectsDisplayRightContainer')
+        const projectsEditIconContainer = document.createElement('div');
+        projectsEditIconContainer.classList.add('projectsEditIconContainer')
+        const projectsEditIcon = document.createElement('i');
         projectsEditIcon.classList.add("fa-solid");
         projectsEditIcon.classList.add("fa-pen");
-        projectsEditIcon.setAttribute("id", "projectsEditIcon");
+        projectsEditIcon.setAttribute("id", "projectsEditIcon")
         const projectsDeleteButton = document.createElement("button");
         projectsDeleteButton.classList.add("projectsDeleteButton");
         projectsDeleteButton.textContent = "Del";
@@ -77,20 +83,14 @@ export const Display = (() => {
         projectsDisplayRightContainer.appendChild(projectsEditIconContainer);
         projectsEditIconContainer.appendChild(projectsEditIcon);
         projectsDisplayRightContainer.appendChild(projectsDeleteButton);
-
+  
         const renderEditProjectModal = (() => {
           projectsDisplay.addEventListener("click", (e) => {
-            if (
-              e.target.className === "projectsText" ||
-              e.target.id === "projectsEditIcon"
-            ) {
+            if (e.target.className === "projectsText" || e.target.id === "projectsEditIcon") {
               editProjectTitle.value =
-                projectsArray[projectsArray.indexOf(element)].title;
+                element.title;
               editProjectModalContainer.classList.add("show");
-              editSubmitProject.setAttribute(
-                "data-id",
-                e.target.parentNode.dataset.id
-              );
+              editSubmitProject.setAttribute("data-id", e.target.closest('.projectsDisplay').dataset.id);
             }
           });
         })();
@@ -106,54 +106,40 @@ export const Display = (() => {
         projectsSidebarButton.textContent = element.title;
         projectsSidebarButton.setAttribute(
           "data-id",
-          projectsArray[projectsArray.indexOf(element)].id
+          element.id.toString()
         );
         projectsContainer.appendChild(projectsSidebarButton);
-        console.log('test');
-        console.log(projectsContainer.firstChild);
-        console.log(projectsSidebarButton);
       });
     },
-    renderOneProject: (doc) => {
-      const projectsSidebarButton = document.createElement("button");
-      projectsSidebarButton.classList.add("projectsSidebarButton");
-      projectsSidebarButton.textContent = doc.data().title;
-      projectsSidebarButton.setAttribute(
-        "data-id",
-        doc.id
-      );
-      projectsContainer.appendChild(projectsSidebarButton);
-      console.log('test');
-      console.log(projectsContainer.firstChild);
-      console.log(projectsSidebarButton);
+    renderCurrentProject(currentProject) {
+      mainContainerTopText.textContent = currentProject.title;
     },
-
-    renderOneTask: (doc) => {
+    renderTasks(taskArray) {
       while (mainContentContainer.firstChild) {
         mainContentContainer.removeChild(mainContentContainer.firstChild);
       }
+      taskArray.forEach((element) => {
         const tasksDisplay = document.createElement("div");
         tasksDisplay.classList.add("tasksDisplay");
         tasksDisplay.setAttribute(
           "data-id",
-          doc.id
-        );
+          element.id.toString());
         mainContentContainer.appendChild(tasksDisplay);
-        const tasksDisplayLeftContainer = document.createElement("div");
-        tasksDisplayLeftContainer.classList.add("tasksDisplayLeftContainer");
+        const tasksDisplayLeftContainer = document.createElement('div');
+        tasksDisplayLeftContainer.classList.add('tasksDisplayLeftContainer')
         const tasksText = document.createElement("div");
         tasksText.classList.add("tasksText");
-        tasksText.textContent = doc.data().title;
+        tasksText.textContent = element.title;
         const tasksCheckBox = document.createElement("div");
         tasksCheckBox.classList.add("tasksCheckBox");
-        const tasksDisplayRightContainer = document.createElement("div");
-        tasksDisplayRightContainer.classList.add("tasksDisplayRightContainer");
-        const tasksEditIconContainer = document.createElement("div");
-        tasksEditIconContainer.classList.add("tasksEditIconContainer");
+        const tasksDisplayRightContainer = document.createElement('div');
+        tasksDisplayRightContainer.classList.add('tasksDisplayRightContainer')
+        const tasksEditIconContainer = document.createElement('div');
+        tasksEditIconContainer.classList.add('tasksEditIconContainer');
         const tasksEditIcon = document.createElement("span");
         tasksEditIcon.classList.add("fa-solid");
         tasksEditIcon.classList.add("fa-pen");
-        tasksEditIcon.setAttribute("id", "tasksEditIcon");
+        tasksEditIcon.setAttribute("id", "tasksEditIcon")
         const tasksDeleteButton = document.createElement("button");
         tasksDeleteButton.classList.add("tasksDeleteButton");
         tasksDeleteButton.textContent = "Del";
@@ -168,8 +154,7 @@ export const Display = (() => {
         dropdownContent.classList.add("dropdownContent");
         dropdownContent.setAttribute(
           "data-dropdown",
-          doc.id
-        );
+          element.id.toString());
         const lowPriority = document.createElement("a");
         lowPriority.textContent = "low";
         lowPriority.href = "#";
@@ -197,29 +182,23 @@ export const Display = (() => {
         dropdownContent.appendChild(highPriority);
         dropdownContainer.appendChild(dropdown);
         tasksDisplayRightContainer.appendChild(tasksDeleteButton);
-
+  
         const addDropdownListeners = (() => {
           dropdownButton.addEventListener("click", (e) => {
             if (e.target.className === "dropdownButton") {
-              document
-                .querySelector(
-                  `[data-dropdown="${taskArray[
-                    taskArray.indexOf(element)
-                  ].id.toString()}"]`
-                )
-                .classList.toggle("showDropdown");
+              document.querySelector(`[data-dropdown="${element.id.toString()}"]`).classList.toggle("showDropdown");
             }
           });
         })();
         const displayCompleteStatus = (() => {
-          if (doc.data().complete === true) {
+          if (element.complete === true) {
             tasksText.style.textDecoration = "line-through";
             tasksCheckBox.style.backgroundColor = "#32fbe2";
           }
         })();
-
+  
         const displayPriorityStatus = (() => {
-          switch (doc.data().priority) {
+          switch (element.priority) {
             case "low":
               dropdownButton.style.backgroundColor = "green";
               break;
@@ -233,25 +212,25 @@ export const Display = (() => {
         })();
         const renderEditTaskModal = (() => {
           tasksDisplay.addEventListener("click", (e) => {
-            if (
-              e.target.className === "tasksText" ||
-              e.target.id === "tasksEditIcon"
-            ) {
-              editTaskTitle.value = doc.data.title;
+            if (e.target.className === "tasksText" || e.target.id === "tasksEditIcon") {
+              editTaskTitle.value =
+                element.title;
               editTaskDescription.value =
-                doc.data().description;
+                element.description;
               editTaskDueDate.value =
-                doc.data().dueDate;
-              editTaskNotes.value = doc.data().notes;
+                element.dueDate;
+              editTaskNotes.value =
+                element.notes;
               editTaskModalContainer.classList.add("show");
               editSubmitTask.setAttribute(
                 "data-id",
-                doc.id
-              );
+                element.id.toString());
             }
           });
         })();
+      });
     },
+
     renderNewProjectModal: () => {
       newProjectButton.addEventListener("click", () => {
         projectModalContainer.classList.add("show");
@@ -294,6 +273,162 @@ export const Display = (() => {
             .classList.remove("show");
         }
       };
+    },
+
+////////////////////EVENT LISTENERS/////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+    handleNewProjectEventListener: (storeProjectInFireStore) => {
+      submitProject.addEventListener("click", (e) => {
+        e.preventDefault();
+        storeProjectInFireStore(projectTitle.value);
+        projectForm.reset();
+        projectModalContainer.classList.remove("show");
+        newTaskButton.classList.remove("hide");
+      })
+    },
+    
+    handleDeleteProjectEventListener:(deleteProject) => {
+      mainContentContainer.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (e.target.className === "projectsDeleteButton") {
+          deleteProject(e.target.closest(".projectsDisplay").dataset.id);
+          e.target.closest(".projectsDisplay").remove();
+        }
+      });
+    },
+    
+    handleEditProjectEventListener:(editProjectInArray) => {
+      editSubmitProject.addEventListener("click", (e) => {
+        e.preventDefault();
+        editProjectInArray(e.target.dataset.id, editProjectTitle.value);
+        editProjectModalContainer.classList.remove("show");
+      });
+    },
+    
+    handleAllProjectsEventListener:(showAllProjects) => {
+      allProjects.addEventListener("click", (e) => {
+        topText.textContent = "All Projects";
+        newTaskButton.style.display = 'none';
+        topText.style.textAlign = 'center;'
+        showAllProjects();
+      });
+    },
+    
+    handleNewTaskEventListener: (storeTaskInFirestore) => {
+      submitTask.addEventListener("click", (e) => {
+        e.preventDefault();
+        storeTaskInFirestore(
+          taskTitle.value,
+          taskDescription.value,
+          taskDueDate.value,
+          taskNotes.value,
+          currentProjectId
+          );
+        taskForm.reset();
+        taskModalContainer.classList.remove("show");
+      });
+    },
+    
+      handleDeleteTaskEventListener:(deleteTaskFromFirestore) => {
+        mainContentContainer.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (e.target.className === "tasksDeleteButton") {
+          deleteTaskFromFirestore(e.target.closest(".tasksDisplay").dataset.id);
+          e.target.closest(".tasksDisplay").remove();
+        }
+      });
+    },
+    
+    handleEditTaskEventListener:(editTaskInFirestore) => {
+      editSubmitTask.addEventListener("click", (e) => {
+        e.preventDefault();
+        editTaskInFirestore(
+          e.target.dataset.id,
+          editTaskTitle.value,
+          editTaskDescription.value,
+          editTaskDueDate.value,
+          editTaskNotes.value
+        );
+        editTaskModalContainer.classList.remove("show");
+        if (
+          topText.textContent === "Today" ||
+          topText.textContent === "All Tasks"
+        ) {
+          newTaskButton.classList.add("hide");
+        } else {
+          newTaskButton.classList.remove("hide");
+        }
+      });
+    },
+    
+    handleAllTasksEventListener:(showAllTasks) => {
+      allTasks.addEventListener("click", (e) => {
+        topText.textContent = "All Tasks";
+        newTaskButton.style.display = 'none';
+        showAllTasks();
+      });
+    },
+    
+    handleUpdateTaskCompleteStatus:(updateTaskCompleteStatus) => {
+      mainContentContainer.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (
+          e.target.className === "tasksCheckBox" &&
+          e.target.style.backgroundColor === ""
+          ) {
+            updateTaskCompleteStatus(
+              e.target.closest(".tasksDisplay").dataset.id,
+              true
+              );
+            } else if (e.target.className === "tasksCheckBox") {
+          updateTaskCompleteStatus(
+            e.target.closest(".tasksDisplay").dataset.id,
+            false
+          );
+        }
+      });
+    },
+    
+    handleUpdateTaskPriority:(updateTaskPriority) => {
+      mainContentContainer.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (e.target.className === "lowPriority") {
+          updateTaskPriority(
+            e.target.closest(".tasksDisplay").dataset.id,
+            "low"
+          );
+        } else if (e.target.className === "mediumPriority") {
+          updateTaskPriority(
+            e.target.closest(".tasksDisplay").dataset.id,
+            "medium"
+          );
+        } else if (e.target.className === "highPriority") {
+          updateTaskPriority(
+            e.target.closest(".tasksDisplay").dataset.id,
+            "high"
+          );
+        }
+      });
+    },
+    
+    handleUpdateTodayTasks:(updateTodayTasks) => {
+      todayButton.addEventListener("click", (e) => {
+        topText.textContent = "Today";
+        newTaskButton.style.display = 'none';
+        updateTodayTasks();
+      });
+    },
+    
+    handleUpdateCurrentProject:(updateCurrentProject) => {
+      projectsContainer.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (e.target.className === "projectsSidebarButton") {
+          newTaskButton.style.display = 'block';
+          currentProjectId = (e.target.dataset.id);
+          updateCurrentProject(e.target.dataset.id);
+          topText.textContent = e.target.textContent;
+        }
+      });
     },
   };
 })()
